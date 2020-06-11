9dc6acda79ae241d84b5d88534b8beef84ee12db
Matt Crinklaw-Vogt <mlaw@fb.com>
2020-06-10 20:57:00 -0400

    fix syntax errors

--- bundle.js
+++ bundle.js
@@ -1,3 +1,6 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+
 /*
 const tile = {
   edges: {
@@ -5,42 +8,15 @@ const tile = {
   },
 };
 */
-
-type ImagePath = string;
-
-type Side = 'top' | 'left' | 'bottom' | 'right';
-type EdgeType = 'water' | 'underground' | 'sky' | 'land_surface' | 'water_surface';
-
-type Tile = $ReadOnly<{
-  edges: $ReadOnly<{
-    top: Edge,
-    left: Edge,
-    bottom: Edge,
-    right: Edge,
-  }>,
-  image: ImagePath,
-}>;
-
 // Edge would need to be an interface because whether or not it connects with another edge is mosty
 // easily modeled and best organized as a method. If the connection logic was separate than how could we
 // pull new edge types into our system?
 // Hmm data edges with a function to `getPartner` may be better as we can have a set of rules for edges that govern pairing
 // and a registry of available edges... to see which ones meet that pairing.
 // Well registry of edge types...
-type Edge = $ReadOnly<{
-  type: EdgeType,
-}>;
-
-type TileMap = $ReadOnly<{
-  [key: EdgeType]: $ReadOnly<{
-    [key: Side]: $ReadOnlyArray<Side>,
-  }>,
-}>;
-
 // We need new in memory storage that has a different concept of identity. Nominal identity -- same name over time but the concrete named changes -- with a link to its concrete instances over time.
 // Nominal identity vs Structural identity.
 // Nominal typing vs structural typing.
-
 // Nomnial identity is that the named thing is always the same thing when recalled by that name even though its particular attributes might change over time.
 // The James river is always the James river even though its banks and volume might shift. Some general properties always hold for nominal identities/
 // When does a nominal identity break? When some essential characteristics no longer hold.
@@ -53,56 +29,45 @@ type TileMap = $ReadOnly<{
 // if it needs to reverse or inspect a past event. Or if history should bear influence over subsequent events. Well shouldn't that history be implicitly captured at all times in the
 // current state of the system at any snapshot in time? Not if the system includes a reasoning being that can look at the past? Well do we look at the past or just artifacts in the present
 // that represent moments in the past.
-
-function generateTile(neighborOne: [Side, Tile], neighborTwo: [Side, Tile]): Tile {
+function generateTile(neighborOne, neighborTwo) {
   // 1. pick random tiles until one fits with left and bottom
   // 2. intersect getTiles(getPartners(leftNeighbor->edges->right)) with getTiles(getPartners(bottomNeighbor->edges->top))
-  return intersect(
-    getTilesFor(
-      getPartnersFor(
-        neighborOne[1].edges[mirror(neighborOne[0])],
-      ),
-    ),
-    getTilesFor(
-      getPartnersFor(
-        neighborTwo[1].edges[mirror(neighborTwo[0])],
-      ),
-    ),
-  );
+  return intersect(getTilesFor(getPartnersFor(neighborOne[1].edges[mirror(neighborOne[0])])), getTilesFor(getPartnersFor(neighborTwo[1].edges[mirror(neighborTwo[0])])));
 }
 
-function mirror(side: Side): Side {
-  return ({
+function mirror(side) {
+  return {
     left: 'right',
     top: 'bottom',
     right: 'left',
-    bottom: 'top',
-  })[side];
+    bottom: 'top'
+  }[side];
 }
 
-function intersect<T>(a: $ReadOnlyArray<T>, b: $ReadOnlyArray<T>): $ReadOnlyArray<T> {
-  const left = new Set(a);
-  return b.filter(x => left.contains(x));
+function intersect(a, b) {
+  var left = new Set(a);
+  return b.filter(function (x) {
+    return left.contains(x);
+  });
 }
 
-function getTilesFor(tileMap: TileMap, edge: Edge, side: Side): $ReadOnlyArray<Tile> {
+function getTilesFor(tileMap, edge, side) {
   // look up available tiles that have the given edge on the given side
   return tileMap[edge][side];
 }
 
-function getPartnersFor(edge: Edge): $ReadOnlyArray<Edge> {
+function getPartnersFor(edge) {
   // edges can only be paired with themselves at the moment
   return [edge];
 }
 
-function loadTileMap(directory: string): TileMap {
-  // read the directory
-  // parse the tile file names which will encode the edges and sides
-}
-
+function loadTileMap(directory) {} // read the directory
+// parse the tile file names which will encode the edges and sides
 // Need the current state of the screen?
 // Need an orientation?
 // Start generating at a given corner?
-function generateScreen(): Screen {
 
-}
+
+function generateScreen() {}
+
+},{}]},{},[1]);
